86dc47c13a184dc7a3bfaf8fb7568b33bada3ceb
C Carr <c.a.carr@exeter.ac.uk>
2025-05-26 13:06:44 +0000

    Consensus algorithm for the blockchain

---
+++ consensus/consensus.py
@@ -0,0 +1,4 @@
+# THIS IS THE CONSENSUS ALGORITHM FOR THE BLOCKCHAIN
+
+# 1. Using your P2P network, decide on how you agree to a new block on the blockchain
+# 2. You will need to agree on the block's contents, and the order of the blocks
